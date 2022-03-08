b39769ed24fc913303da3e1ef3f400f28d306a86
yugahatta <97233326+yugahatta@users.noreply.github.com>
2022-03-08 18:47:55 +0000

    Add files via upload

--- EditProd.php
+++ EditProd.php
@@ -1,56 +1,81 @@
-    <body>
+
+<body>
         <h2>Update Product Information</h2>
         <?php
         
         //estabelecer a conexão á base e dados;
         include 'include/liga_bd.php';
         //valida o acesso atraves das variaveis de sessão
-        include 'include/validar.php';
+        //include 'include/validar.php';
+
+        //include 'include/validate_photo.php';
 
         //recieve id of the produc to update fro Consult.php
-        $id_prod=$_POST['id_prod'];
+        //$id_prod=$_POST['id_prod'];
+
+        $id_prod=1;
 
         //mysql operation to get information of product by id from t-product, with procedure registared in DB 
-        $sql ="Procedure1".$id_prod;
-        $resultado = mysqli_query($ligacao, $sql) or die (mysqli_error($ligacao));
-        $linha = mysqli_fetch_assoc($resultado);
+        mysqli_query($conn, "SET @1=".$id_prod);
+        mysqli_multi_query($conn, "CALL GetProductById(@1)") OR DIE (mysqli_error($conn));
+
+        while(mysqli_more_results($conn)){
+            if($result = mysqli_store_result($conn)){
+                while($row = mysqli_fetch_assoc($result)){
+                    $id = $row['id'];
+                    $name = $row['name'];
+                    $category= $row['idCategory'];
+                    $price = $row['price'];
+                    $stock = $row['stock'];
+                    $photo = $row['photo'];
+                }
+                mysqli_free_result($result);
+            } 
+            mysqli_next_result($conn);
+        }
         ?>
-        
 
-            <!--form to modify information of product and send it tto EditProd2.php when submitted  -->
+        <!--form to modify information of product and send it tto EditProd2.php when submitted  -->
         <form action="EditProd2.php" method="post" enctype="multipart/form-data">
-            ID:<br><input type="text" name="id" size="100%" value="<?php echo $linha['id']?>" readonly>
-            Name:<br><input type="text" name="nick" size="50" value="<?php echo $linha['name']?>">
+            ID:<br><input type="text" name="id" size="10" value="<?php echo $id?>" readonly><br><br>
+            Name:<br><input type="text" name="name" size="50" maxlength="50" value="<?php echo $name?>"><br><br>
 
-            Category: <select name="categoria" id="categoria">
+            Category: <select name="category" id="category">
             <?php
 
             //mysql operation to get all the product categories from t-category, with procedure registared in DB
-            $sql ="Procedure2";
-            $resultado = mysqli_query($ligacao, $sql) or die (mysqli_error($ligacao));
-            $linha = mysqli_fetch_assoc($resultado);
-        
-            //fill select box options with categories
-            while($linha = mysqli_fetch_assoc($resultado)){
-                if ($_POST['categoria']==$linha['id'])
-                    echo "<option value='".$linha['id']."' selected>" .$linha['category']. "</option>";
-                else
-                    echo "<option value='".$linha['id']."'>".$linha['category']."</option>"; 
+            mysqli_multi_query($conn, "CALL GetCategory()") OR DIE (mysqli_error($conn));
+
+            while(mysqli_more_results($conn)){
+                if($result2 = mysqli_store_result($conn)){
+                    while($row2 = mysqli_fetch_assoc($result2)){
+                        if ($category == $row2['id'])
+                            echo "<option value='".$row2['id']."' selected>" .$row2['category']. "</option>";
+                        else
+                            echo "<option value='".$row2['id']."'>".$row2['category']."</option>"; 
+                    }
+                mysqli_free_result($result2);
+                }
+            mysqli_next_result($conn);
             }
-            mysqli_close($ligacao)  
             ?>
 
-            Price:<br><input type="number" name="nome" size="100" value="<?php echo $linha['nome']?>">
-            Stock:<br><input type="number" name="email" size="50" value="<?php echo $linha['stock']?>">
-            Image:<input type="file" name="image"><br><br>
+            </select><br><br>
+            Price:<br><input type="number" name="price" size="20" min="0"  step="0.01" value="<?php echo $price?>"><br><br>
+            Stock:<br><input type="number" name="stock" size="20" min="0" step="1" value="<?php echo $stock?>"><br><br>
+            Image:<br><img src="pics/<?php echo $photo;?>" width="150"><input type="hidden" name="existing_image" value="<?php echo $photo?>"><br>
+            <br>New Image:<br><input type="file" name="image"></p><br>
             
-            </select>
+            <?php
+            mysqli_close($conn) 
+            ?>
 
             <input type="submit" value="Edit"><br><br>
             <input type="reset" value="Clear"><br><br>
             <input type="button" value="Cancel" onclick="window.history.go(-1);"></p><br>
         </form>
-
+        
+        
         <a href="index.html" target="_self">Back to Menu</a>
     </body>
 </html>
--- EditProd2.php
+++ EditProd2.php
@@ -5,29 +5,60 @@
         //estabelecer a conexão á base e dados;
         include 'include/liga_bd.php';
         //valida o acesso atraves das variaveis de sessão
-        include 'include/validar.php';
+        //include 'include/validar.php';
 
-        //validate image and upload
-        include 'include/valida_foto.php';
+       
 
-        if($uploadOk == 0){
-                echo "image failed to upload";
-        }
-        else{
-            if($uploadOk == 1){
+        if(empty($_FILES['image']['name'][0])){
+            
+            //in case there is no update of foto
+            //mysql operation to update the information of the product at t-category, with procedure registared on the DB
+            mysqli_query($conn, "SET @1='".$_POST['name']."'");
+            mysqli_query($conn, "SET @2=".$_POST['price']);
+            mysqli_query($conn, "SET @3=".$_POST['stock']);
+            mysqli_query($conn, "SET @5=".$_POST['category']);
+            mysqli_query($conn, "SET @6=".$_POST['id']);
+            if (mysqli_multi_query($conn, "CALL UpdateProductNP(@1, @2, @3, @4, @5)")){ 
 
-                //mysql operation to update the information of the product at t-category, with procedure registared on the DB
-                $sql ="Procedure3";  
-
-                if(mysqli_query($ligacao, $sql)){
-                    echo "<h3>The product updated</h3>";
-                    move_uploaded_file($_FILES["image"]["tmp_name"], $target_file);
-                }
-                else
-                    echo "<h3>Error<h3>";
+                echo "<h3>The product updated</h3>";
+            }
+            else{
+                DIE (mysqli_error($conn));
+                echo "<h3>Error<h3>";
             }
         }
-        mysqli_close($ligacao);  
+        else
+        {       
+             //validate image and upload
+            include 'include/validate_photo.php';
+            if($uploadOk == 0){
+                echo "image failed to upload";
+            }
+            else{
+                if($uploadOk == 1){
+
+                    //mysql operation to update the information of the product at t-category, with procedure registared on the DB
+                    mysqli_query($conn, "SET @1='".$_POST['name']."'");
+                    mysqli_query($conn, "SET @2=".$_POST['price']);
+                    mysqli_query($conn, "SET @3=".$_POST['stock']);
+                    mysqli_query($conn, "SET @4='".$_POST['image']."'");
+                    mysqli_query($conn, "SET @5=".$_POST['category']);
+                    mysqli_query($conn, "SET @6=".$_POST['id']);
+                    if (mysqli_multi_query($conn, "CALL UpdateProduct(@1, @2, @3, @4, @5, @6)")){ 
+
+            
+                        echo "<h3>The product updated</h3>";
+                        move_uploaded_file($_FILES["image"]["tmp_name"], $target_file);
+                    }
+                    else{
+                        DIE (mysqli_error($conn));
+                        echo "<h3>Error<h3>";
+                    }
+                }
+            }
+        }
+
+        mysqli_close($conn);  
         ?>
         
         <p>Will be redirected to Menu</p>
