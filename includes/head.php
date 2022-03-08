e646680cea2c12bbb87b3a70d24ca3a4db17db86
Jvtrovao <37725183+Jvtrovao@users.noreply.github.com>
2022-03-08 15:50:51 +0100

    Add files via upload

---
+++ includes/head.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <meta charset="utf-8">
+        <title>LIDL</title>
+        <link href="resources/style/style.css" rel="stylesheet" type="text/css">
+    </head>
