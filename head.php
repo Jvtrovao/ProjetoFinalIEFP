afac0ad0ccf678636c2867bf65aa7ff05078bd3d
hmvcarvalho <100861615+hmvcarvalho@users.noreply.github.com>
2022-03-15 14:57:45 +0000

    Add files via upload
    Ficheiro head.php com os includes de bootstrap e js

---
+++ head.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <meta charset="utf-8">
+        <title>MARKET</title>
+        <link href="resources/style/style.css" rel="stylesheet" type="text/css">
+        <script src="resources/js/myCode.js"></script>
+        <!--<meta name="viewport" content="width-device-width, initial-scale-1">
+        <link href="css/bootstrap.min.css" rel="stylesheet"/>
+        <script src="js/bootstrap.bundle.js"></script>-->
+        <!--insert bootstrap-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    </head>
